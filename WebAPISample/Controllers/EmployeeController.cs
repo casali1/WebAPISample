4ecadeceb0bfc1fe9f061d32f6d06d3935cd5ed1
casali1 <frank_casali@hotmail.com>
2018-08-18 17:11:06 -0600

    Add project files.

---
+++ WebAPISample/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+
+        public Employee GET()
+        {
+            Employee e = new Employee();
+            e.FirstName = "Sukesh";
+            e.LastName = "Marla";
+            e.Salary = 25000;
+            return e;
+        }
+
+    }
+}
---
+++ WebAPISample/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISample.Models
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Designation { get; set; }
+    }
+
+
+}
